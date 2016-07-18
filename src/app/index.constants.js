0df5ea3520457b4afbd37ad30fe8a44bb24bb273
Reka Karoly <reka.karoly@accenture.com>
2016-07-18 10:54:43 +0300

    Project skeleton.

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('houseRental')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
