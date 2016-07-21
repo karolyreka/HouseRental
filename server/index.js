844116b06cf20bf120e90e482e6ea30050ebfac8
Reka Karoly <reka.karoly@accenture.com>
2016-07-21 15:51:21 +0300

    Missing package added to packages json. post changed in server.js headers.

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8082');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
